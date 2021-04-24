ce7739d7d3f61c5085eee493b312705d6430ead5
Ushitaku <doping0815@gmail.com>
2021-04-24 15:43:03 +0900

    blog調整

---
+++ pages/blogLink.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { chakra, Box, Flex, Button, Link, Image } from "@chakra-ui/react";
+
+
+const BlogLink = () => {
+    return (
+        <>
+        <Box mb="40" textAlign="center">
+        <Link href="posts">
+        <Button >続きを見る</Button>
+        </Link>
+        </Box>
+        </>
+    )
+}
+
+export default BlogLink
---
+++ pages/posts/[id].js
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+import {Box, Flex, Image, } from '@chakra-ui/react'
+
+export default function Post({ postData }) {
+    return (
+    <Layout>
+        <Head>
+        <title>{postData.title}</title>
+        </Head>
+        <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      <Box>
+         <Flex>
+        <Box >
+        <Image p="3" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/facebook_erc9dv.png'} alt='Facebook'
+                width={''} height={''} />
+        </Box>
+        <Box>
+        <Image p="3" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/twitter_wzmz9k.png'} alt='Twitter'
+                width={''} height={''} />
+        </Box>
+        <Box>
+        <Image p="3" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/instergram_oug8df.png'} alt='Instagram'
+                width={''} height={''} />
+        </Box>
+        </Flex>
+      </Box>
+    </Layout>
+    )
+  }
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+      paths,
+      fallback: false
+    }
+  }
