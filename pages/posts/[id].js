75db155fe20d8f67de8aef92ace31e0ef6478ea9
Ushitaku <doping0815@gmail.com>
2021-04-29 16:51:36 +0900

    ogp add

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,32 +4,36 @@ import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 import {Box, Flex, Image,Link,Button } from '@chakra-ui/react'
+import {InstapaperShareButton, TwitterShareButton, FacebookShareButton,} from "react-share";
 
 export default function Post({ postData }) {
     return (
     <Layout>
       <Head>
         <link rel="icon" href="https://res.cloudinary.com/telabel/image/upload/v1618286838/fabicon_jr2jqg.png" />
-      </Head>
-        <Head>
         <title>{postData.title}</title>
-        </Head>
-        <article>
+      </Head>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <h2>{postData.location}</h2>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
 
       <Flex justifyContent="flex-end" m='4' mt="4" >
+        <FacebookShareButton url={`https://telabel-official-q1x2tx72h-ushitaku.vercel.app/posts/${postData.id}`}>
         <Image p="" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/facebook_erc9dv.png'} alt='Facebook'
                 width={''} height={''} />
+        </FacebookShareButton>
+
+        <TwitterShareButton url={`https://telabel-official-q1x2tx72h-ushitaku.vercel.app/posts/${postData.id}`} title={postData.title}>
         <Image p="" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/twitter_wzmz9k.png'} alt='Twitter'
                 width={''} height={''} />
+        </TwitterShareButton>
+        {/* <InstapaperShareButton url={`https://telabel-official-q1x2tx72h-ushitaku.vercel.app/posts/${postData.id}`}>
         <Image p="" src={'https://res.cloudinary.com/telabel/image/upload/v1618401644/instergram_oug8df.png'} alt='Instagram'
                 width={''} height={''} />
+        </InstapaperShareButton> */}
       </Flex>
           <Link href="/posts">
             <Button>← 記事一覧に戻る</Button>
